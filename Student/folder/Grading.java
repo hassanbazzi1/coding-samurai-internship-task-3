a503ec3cb250bd6dcb2f4bc3bb970258cbd2846a
Hassan Bazzi <133389011+hassanbazzi1@users.noreply.github.com>
2023-10-04 20:21:07 +0300

    Grading.java

--- Student/folder/Grading.java
+++ Student/folder/Grading.java
@@ -4,10 +4,7 @@ public class Grading implements File{
     private float[] assignmentGrades;
     private float[] quizGrades;
     private float[] examGrades;
-
     private float finalExamGrade;
-
-
     @Override
     public void GetGrades() {
         // Here we enter the number of assignments and the grade of each
@@ -19,8 +16,6 @@ public class Grading implements File{
         for (int i = 0; i < numAssignments; i++) {
             assignmentGrades[i] = scamner.nextFloat();
         }
-
-
         // Here we enter the number of quizzes and the grade of each quiz
         System.out.println("enter the number of your quizzes:");
         int quiznumbers = scamner.nextInt();
@@ -29,7 +24,6 @@ public class Grading implements File{
         for (int i = 0; i < quiznumbers; i++) {
             quizGrades[i] = scamner.nextFloat();
         }
-
         //here we enter the number of exams and the grades of each exam
         System.out.println("enter the number of exams: ");
         int examnumbers = scamner.nextInt();
@@ -38,17 +32,10 @@ public class Grading implements File{
         for (int i = 0; i < examnumbers; i++) {
             examGrades[i] = scamner.nextFloat();
         }
-
         //here we enter the grade of the final exam
         System.out.println("Enter the grade of the final exam:");
         finalExamGrade = scamner.nextFloat();
-
-
     }
-
-
-
-
     @Override
     public void GradeScale() {
         float coursegrade = CalculateGrade();
@@ -68,12 +55,6 @@ public class Grading implements File{
         System.out.println("The Final Course Grade: "+coursegrade);
         System.out.println("The Grade Scale: "+letterGrade);
     }
-
-
-
-
-
-
     public float CalculateGrade() {
         float assignmentAvg = calculateAverage(assignmentGrades);
         float quizAvg = calculateAverage(quizGrades);
@@ -82,11 +63,6 @@ public class Grading implements File{
         // Calculate the overall course grade
         return 0.3f * assignmentAvg + 0.2f * quizAvg + 0.4f * examAvg + 0.1f * finalExamGrade;
     }
-
-
-
-
-
     private float calculateAverage(float[] grades) {
         if (grades == null || grades.length == 0) {
             return 0.0f;
