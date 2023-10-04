d07c238a051076cb33ae2a72b3628aceb3f6d52c
Hassan Bazzi <133389011+hassanbazzi1@users.noreply.github.com>
2023-10-04 20:20:16 +0300

    Main.java

--- Student/folder/Main.java
+++ Student/folder/Main.java
@@ -4,16 +4,13 @@ public class Main {
     public static void main(String[] args) {
         Grading grade = new Grading();
         Scanner scanner = new Scanner(System.in);
-
         while(true){
             System.out.println("\n Options:");
             System.out.println("1.Input Grades");
             System.out.println("2.Calculate and Display Course Grade");
             System.out.println("3.Exit");
-
             int choice = scanner.nextInt();
-            scanner.nextLine();  // Consume the newline character
-
+            scanner.nextLine(); 
             switch(choice){
                 case 1:
                     grade.GetGrades();
